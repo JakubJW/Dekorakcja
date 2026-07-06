24c5543a6fe07ae2963edeb7dc832161032d9990
Jakub Wysocki <jakub.2115.wysocki@gmail.com>
2026-07-06 21:34:35 +0200

    Add seed properties

--- src/endpoints/seed/index.ts
+++ src/endpoints/seed/index.ts
@@ -1,15 +1,15 @@
-import type { CollectionSlug, GlobalSlug, Payload, PayloadRequest, File } from 'payload'
+import type { CollectionSlug, File, GlobalSlug, Payload, PayloadRequest } from 'payload'
 
+import { Address, Transaction, VariantOption } from '@/payload-types'
 import { contactFormData } from './contact-form'
 import { contactPageData } from './contact-page'
-import { productHatData } from './product-hat'
-import { productTshirtData, productTshirtVariant } from './product-tshirt'
 import { homePageData } from './home'
 import { imageHatData } from './image-hat'
+import { imageHero1Data } from './image-hero-1'
 import { imageTshirtBlackData } from './image-tshirt-black'
 import { imageTshirtWhiteData } from './image-tshirt-white'
-import { imageHero1Data } from './image-hero-1'
-import { Address, Transaction, VariantOption } from '@/payload-types'
+import { productHatData } from './product-hat'
+import { productTshirtData, productTshirtVariant } from './product-tshirt'
 
 const collections: CollectionSlug[] = [
   'categories',
@@ -91,15 +91,22 @@ export const seed = async ({
   await Promise.all(
     globals.map((global) =>
       payload.updateGlobal({
-        slug: global,
-        data: {
-          navItems: [],
-        },
+        slug: 'footer',
+        data: {},
         depth: 0,
         context: {
           disableRevalidate: true,
         },
       }),
+
+      payload.updateGlobal({
+        slug: 'header',
+        data: { navItems: [] },
+        depth: 0,
+        context: {
+          disableRevalidate: true,
+        },
+      })
     ),
   )
 
